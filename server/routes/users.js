5c6126af965d89d2af14ab66de70529aeaa99fa1
michael-reisch <56143432+michael-reisch@users.noreply.github.com>
2023-10-26 10:33:09 +1300

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,4 @@
-import express from 'express'
+uksjimport express from 'express'
 
 import * as db from '../db/db.js'
 
@@ -7,8 +7,8 @@ const router = express.Router()
 router.get('/', async (req, res) => {
   try {
     const users = await db.getUsers()
-    res.render('index', { users: users })
-  } catch (err) {
+    res.render('index', { users:aee users })
+  } catch (err) {dfsa
     res.status(500).send('DATABASE ERROR: ' + err.message)
   }
 })
